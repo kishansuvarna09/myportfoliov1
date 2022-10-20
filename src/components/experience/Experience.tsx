3a595005528b37dda079f401e5eff210eb9ada42
Kishan Suvarna <kishansuvarna09@gmail.com>
2022-10-21 00:02:47 +0530

    Experiences page

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import "./experience.css";
+import ExperienceCard from "./ExperienceCard";
+import { frontendExperiences, backendExperiences } from "./data";
 
 type Props = {};
 
 const experience = (props: Props) => {
-  return <section id="experience">experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <ExperienceCard
+          title="Frontend Development"
+          experiences={frontendExperiences}
+        />
+        <ExperienceCard
+          title="Backend Development"
+          experiences={backendExperiences}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default experience;
