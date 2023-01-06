b67d1f8cf45b0b4bd77c812610d6224a476fa9e3
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-01-06 11:52:28 +0530

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,33 +3,6 @@
  */
 const nextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' 'http://kishansuvarna.in';",
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
