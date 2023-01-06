070bacf88ea3ef050ecf2a4da7d1c05fef084062
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-01-06 11:47:58 +0530

    updated next.config.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,4 @@ function IndexPage() {
   return <></>;
 }
 
-export async function getServerSideProps({ res }) {
-  res.writeHead(307, { Location: '/portfolio' });
-  res.end();
-  return { props: {} };
-}
-
 export default IndexPage;
